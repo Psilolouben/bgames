83455a13be1b09ac8ef90316c973bf82332d88b8
Markos Rigas <marky.rigas@gmail.com>
2021-03-08 21:08:19 +0200

    Fine tune every day diff

--- app/models/bgame.rb
+++ app/models/bgame.rb
@@ -5,6 +5,9 @@ require 'curb'
 class Bgame < ActiveRecord::Base
   scope :starting_with_a, -> { where('name like \'A%\'') }
   scope :todays, -> { where('created_at BETWEEN ? AND ?', DateTime.now.beginning_of_day, DateTime.now.end_of_day) }
+  scope :in_stock, -> { where(in_stock: true) }
+
+  attr_accessor :bgames
 
   def self.crawl_games
     # Fetch in stock
@@ -13,50 +16,64 @@ class Bgame < ActiveRecord::Base
     # Set out of stock
   end
 
+  def self.get_diff
+    @bgames = Bgame.all.to_a
+    previous = Bgame.in_stock.to_a
+    fetch_in_stock
+    current = Bgame.in_stock.to_a
+
+    previous - current | current - previous
+  end
+
   private
 
   def self.fetch_in_stock
+    Bgame.update_all(in_stock: false)
+
     @iteration = 1
-    @bgames = []
 
     while @iteration <= 20
       puts "Now iterating page: " + @iteration.to_s + " at #{Time.now.to_s}"
 
-      url = "https://www.thegamerules.com/epitrapezia-paixnidia?limit=10&fq=1&page=#{@iteration}"
+      url = "https://www.thegamerules.com/epitrapezia-paixnidia?limit=100&fq=1&page=#{@iteration}"
       url_parsed = URI.parse(url)
       response = Net::HTTP.get_response(url_parsed)
 
       response.body.split('<div class="name">').each do |i|
         begin
-        game_name = i.split('</a></div><div')[0].split('>')[1].encode("UTF-8").gsub("(Exp)", "")
-          .gsub("(Exp.)", "").gsub(/\(.*\)/, "").gsub("&amp;", "")
-          .gsub("(","").gsub(")","").gsub(": ", ":").strip.squish.encode('utf-8')
+          game_name = i.split('</a></div><div')[0].split('>')[1].encode("UTF-8").gsub("(Exp)", "")
+            .gsub("(Exp.)", "").gsub(/\(.*\)/, "").gsub("&amp;", "")
+            .gsub("(","").gsub(")","").gsub(": ", ":").strip.squish.encode('utf-8')
 
-        next if game_name.empty?
+          next if game_name.blank?
 
-        # Get from existing if exists
-        game_id = fetch_game_id(game_name)
-        game_info = fetch_game_info(game_id)
-        Bgame.create(
-          name: game_name,
-          bgg_id: game_id,
-          voters: game_info[:voters],
-          score: game_info[:score],
-          in_stock: true
-        )
+          # Get from existing if exists
+          game_id = fetch_game_id(game_name)
+          game_info = fetch_game_info(game_id)
+
+          current_game = Bgame.find_or_initialize_by(id: game_id)
+          current_game.assign_attributes(
+            name: game_name,
+            bgg_id: game_id,
+            voters: game_info[:voters],
+            score: game_info[:score],
+            in_stock: true
+          )
+
+          current_game.save!
         rescue => e
-          puts "Missing info for #{game_name}"
+          File.open('./missing.log', 'a') { |file| file.write("Missing info for #{game_name}\n") }
           next
         end
       end
       @iteration += 1
     end
-
-    @bgames
   end
 
   def self.fetch_game_id(name)
-    #Bgame.where(name: name).any
+    existing = @bgames.select{|n| n.name == name}&.first
+    return existing.id if existing
+
     response = HTTParty.get('https://www.boardgamegeek.com/xmlapi2/search?query=' + CGI.escape(name) + "&type=boardgame").body
     sleep(1)
     hsh = Hash.from_xml(response.gsub("\n", ""))
@@ -68,6 +85,9 @@ class Bgame < ActiveRecord::Base
   end
 
   def self.fetch_game_info(id)
+    existing = @bgames.select{|n| n.bgg_id == id}&.first
+    return { voters: existing.voters, score: existing.score } if existing
+
     response = HTTParty.get('https://www.boardgamegeek.com/xmlapi2/thing?id=' + id.to_s + "&stats=1").body
     hsh = Hash.from_xml(response.gsub("\n", ""))
     rating = hsh["items"]["item"]["statistics"]["ratings"]["average"]["value"].to_f
--- lib/tasks/bgame.rake
+++ lib/tasks/bgame.rake
@@ -1,7 +1,6 @@
 namespace :bgame do
   desc "TODO"
   task fetch_games: :environment do
-    BgamesController.new.crawl_games
     BgamesMailer.todays_games_email.deliver!
   end
 end
